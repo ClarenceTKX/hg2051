ef636942e8a02798b0dcf1708080305ff8b24116
Clarence <69084578+ClarenceTKX@users.noreply.github.com>
2023-02-21 16:26:50 +0800

    21/02/23 updates to file: pre-test commit

--- seminar3_prepcode.py
+++ seminar3_prepcode.py
@@ -10,6 +10,7 @@ print('There are ', len(words), ' words in Moby Dick.')
 
 average = float(len(words)/len(sentences))
 print(average)
+
 # task 2: NLTK sampling
 # i = 0
 # temp = []
@@ -39,11 +40,44 @@ unique_tokens = list(set([j for i in sentences for j in i.split()]))
 
 print(len(unique_tokens))
 
+# task 3: NLTK frequency distribution
 # stopwords = stopwords.words('english')
 # sansSW = []
-# for any(word in unique_tokens for word in stopwords):
-#     sansSW.append(word)
-
+# for word in unique_tokens:
+#     if word not in stopwords:
+#         sansSW.append(word)
+#
 # fdist = nltk.FreqDist(moby.split())
 # print(fdist)
 # print(fdist.most_common(50))
+
+#task 4: case normalised tokens in the book
+moby_lower = moby.lower()
+moby_tokens = moby_lower.split()
+print(f'There are {len(moby_tokens)} case-normalised tokens in Moby Dick.')
+
+#task 5: case normalised tokens in the book, excluding stopwords
+stopwords = stopwords.words('english')
+sansSW = []
+for word in moby_tokens:
+    if word not in stopwords:
+        sansSW.append(word)
+print(f'There are {len(sansSW)} case-normalised tokens without stopwords in Moby Dick. Hence, there are {len(moby_tokens) - len(sansSW)} stopwords in Moby Dick.')
+
+#task 6: set of tokens in the book that start with 'whale'
+whale_tokens = []
+for word in moby_tokens:
+    if word.startswith('whale'):
+        whale_tokens.append(word)
+# filtered for non-unique tokens
+whale_tokens = list(set(whale_tokens))
+print(whale_tokens[:50])
+
+#task 7: set of tokens in the book that start with 'whale' and is alphabetic
+whale_tokens = []
+for word in moby_tokens:
+    if word.startswith('whale') and word.isalpha():
+        whale_tokens.append(word)
+# filtered for non-unique tokens
+whale_tokens = list(set(whale_tokens))
+print(whale_tokens[:50])
